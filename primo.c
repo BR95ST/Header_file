03230d0e105fc0fc2fe07591047e6fd2b8cda06f
Bruno <br95st@protonmail.com>
2017-09-08 19:43:41 -0300

    Update primo.c

--- primo.c
+++ primo.c
@@ -1,5 +1,6 @@
 #include "primo.h"
 
+/*  A função recebe um número e retorna 1 em caso de primo ou 0 se o número não for primo.*/
 int primo(int x){
 	int i, cont = 0;
 	for(i=1;i<x+1;i++){
@@ -14,7 +15,9 @@ int primo(int x){
 		return 0;
 	}
 }
-
+/* A função recebe um número e devolve sua posição entre os números primos.
+   EX: caso a função receba o número 139 será retornado o valor 34, pois o numero 139 é o 34º primo.
+*/
 int indexPrimo(int x){
 	int index = 0, i = 1;
 	if(primo(x)){
