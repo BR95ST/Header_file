fe29d7979e6d1606c9bac98b88129224584124aa
Bruno <br95st@protonmail.com>
2017-09-13 14:22:51 -0300

    Update geo.h

--- geo.h
+++ geo.h
@@ -3,7 +3,6 @@ float area_retangulo(float b, float h);
 float area_losango(float d1, float d2);
 float area_trapezio(float b1, float b2, float h);
 float area_triangulo(float b, float h);
-float area_trianguloRet(float b, float c);
 float area_trianguloEqui(float l);
 float area_circulo(float r);
 float area_coroaCir(float r1, float r2);
